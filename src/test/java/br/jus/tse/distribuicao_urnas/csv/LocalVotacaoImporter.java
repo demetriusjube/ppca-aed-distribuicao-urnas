2bfdb798a1edb70926cc230e45efe7aec21cc591
Demétrius de Almeida Jubé <jube@senado.leg.br>
2022-08-17 15:09:10 -0300

    Ajustes no csv de importação

--- src/test/java/br/jus/tse/distribuicao_urnas/csv/LocalVotacaoImporter.java
+++ src/test/java/br/jus/tse/distribuicao_urnas/csv/LocalVotacaoImporter.java
@@ -1,19 +1,34 @@
 package br.jus.tse.distribuicao_urnas.csv;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 
-import br.jus.tse.distribuicao_urnas.service.LocalVotacaoService;
+import com.opencsv.bean.CsvToBeanBuilder;
 
 @SpringBootTest
 public class LocalVotacaoImporter {
-	
+
 	@Autowired
-	private LocalVotacaoService localVotacaoService;
+	private LocalVotacaoCSVService localVotacaoCSVService;
 
 	@Test
-	public void importarLocaisVotacao() {
-		
+	public void importarLocaisVotacao() throws IllegalStateException, FileNotFoundException, IOException {
+		List<LocalVotacaoCSVDto> locaisVotacaoCSV = new CsvToBeanBuilder(new FileReader(getArquivoCSV()))
+				.withType(LocalVotacaoCSVDto.class).withQuoteChar('\"').build().parse();
+		for (LocalVotacaoCSVDto localVotacaoCSVDto : locaisVotacaoCSV) {
+			localVotacaoCSVService.saveLocalVotacaoFromCSV(localVotacaoCSVDto);
+		}
+	}
+
+	private File getArquivoCSV() throws IOException {
+		return new ClassPathResource("csv/locais-votacao-transporte.csv").getFile();
 	}
 }
